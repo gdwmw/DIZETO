2161dcaac3e3e01180371645e7a611fc7ac083c9
gdwmw <gededewowahyumw@gmail.com>
2023-10-07 16:12:06 +0700

    feat - About | Main | Landing Page

--- src/components/landingPage/Main.tsx
+++ src/components/landingPage/Main.tsx
@@ -1,3 +1,23 @@
+// IMPORT LIBRARIES
+import Image from "next/image";
+
+// IMPORT COMPONENTS
+import imgDIZETO from "@/assets/images/logo/dizeto.svg";
+
+type PaperProps = {
+  id: string;
+  children: React.ReactNode;
+};
+
+// PAPER COMPONENTS
+const Paper = ({ id, children }: PaperProps) => (
+  <section id={id}>
+    <div className="h-fit w-full rounded-xl border border-black/50 bg-white p-5 shadow-md shadow-black/50 dark:border-white/50 dark:bg-dark dark:text-white dark:shadow-white/50">
+      {children}
+    </div>
+  </section>
+);
+
 export default function Main() {
   return (
     <main>
@@ -7,6 +27,33 @@ export default function Main() {
           <span className="text-red-600">DI</span>ZETO
         </h1>
         <p className="text-xl font-semibold">PRODUCTION • ENTERTAINMENT • TALENT • MUSIC RECORD</p>
+      </section>
+
+      <section className="container mx-auto space-y-10 px-5">
+        {/* ABOUT */}
+        <Paper id="About">
+          <h2 className="text-center text-3xl font-semibold">
+            ABO<span className="text-red-600">UT</span>
+            <div className="mx-auto h-px w-20 bg-red-600" />
+          </h2>
+
+          <div className="my-10 space-y-14 md:grid md:grid-cols-2 md:space-y-0">
+            <div className="space-y-5">
+              <h3 className="text-2xl font-semibold">
+                <span className="text-red-600">W</span>hat is Dizeto?
+              </h3>
+              <p className="text-justify">
+                <span className="text-red-600">Dizeto</span> is a vendor that offers photography, videography, talent, and music services. We have a
+                professional team that can help you meet business needs, events, and your special moments to make them more beautiful, real, and
+                lasting. You can learn more about us by viewing our portfolio, YouTube channel, and customer testimonials.
+              </p>
+              <p className="pt-5 text-red-600">*Please take note that we currently only offer photography and videography services.</p>
+            </div>
+            <div className="flex h-full w-full items-center justify-center">
+              <Image src={imgDIZETO} alt="DIZETO" height={250} width={250} />
+            </div>
+          </div>
+        </Paper>
       </section>
     </main>
   );
