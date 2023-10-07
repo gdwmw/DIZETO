4458b5e273ac8590e04e8f6cf1407729edbec00d
gdwmw <gededewowahyumw@gmail.com>
2023-10-08 04:29:07 +0700

    style - Jumbotron | Main | Landing Page

--- src/components/landingPage/Main.tsx
+++ src/components/landingPage/Main.tsx
@@ -25,7 +25,7 @@ export default function Main() {
   return (
     <main>
       {/* JUMBOTRON */}
-      <section id="Jumbotron" className="container mx-auto flex h-[400px] w-full flex-col items-center justify-center gap-3 px-5 dark:text-white">
+      <section id="Jumbotron" className="container mx-auto flex h-[600px] w-full flex-col items-center justify-center gap-3 px-5 dark:text-white">
         <h1 className="text-8xl font-semibold">
           <span className="text-red-600">DI</span>ZETO
         </h1>
