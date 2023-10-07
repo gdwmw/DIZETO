0cbc64c98a0bca5bfd4d6a8c961ef4fad5781ac8
gdwmw <gededewowahyumw@gmail.com>
2023-10-07 15:53:08 +0700

    feat - Jumbotron | Main | Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 // IMPORT COMPONENTS
 import Header from "@/components/landingPage/Header";
+import Main from "@/components/landingPage/Main";
 
 export default function page() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
---
+++ src/components/landingPage/Main.tsx
@@ -0,0 +1,13 @@
+export default function Main() {
+  return (
+    <main>
+      {/* JUMBOTRON */}
+      <section id="Jumbotron" className="container mx-auto flex h-[400px] w-full flex-col items-center justify-center gap-3 px-5 dark:text-white">
+        <h1 className="text-8xl font-semibold">
+          <span className="text-red-600">DI</span>ZETO
+        </h1>
+        <p className="text-xl font-semibold">PRODUCTION • ENTERTAINMENT • TALENT • MUSIC RECORD</p>
+      </section>
+    </main>
+  );
+}
