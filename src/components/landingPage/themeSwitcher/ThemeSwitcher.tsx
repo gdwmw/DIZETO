45f877b72ab94aa373e5907d338652459e458750
gdwmw <gededewowahyumw@gmail.com>
2023-10-13 00:14:15 +0700

    update - Theme Switcher | Header | Landing Page

--- src/components/landingPage/themeSwitcher/ThemeSwitcher.tsx
+++ src/components/landingPage/themeSwitcher/ThemeSwitcher.tsx
@@ -1,4 +1,5 @@
 import { useTheme } from "next-themes";
+import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import { BsFillMoonStarsFill, BsSunFill } from "react-icons/bs";
 import { SiMoonrepo } from "react-icons/si";
@@ -24,7 +25,11 @@ export default function ThemeSwitcher() {
   }, []);
 
   if (!mounted) {
-    return <div className="fixed right-10 top-3 z-[21] h-10 w-10" />;
+    return (
+      <div className="fixed right-10 top-3 z-[21] h-10 w-10">
+        <Image src={require("@/assets/loading/loading.svg")} alt="Loading" height={40} width={40} />
+      </div>
+    );
   }
 
   return (
