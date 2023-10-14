77c792b643469a57df0ca41f27656cb77ef54798
gdwmw <gededewowahyumw@gmail.com>
2023-10-15 02:14:05 +0700

    style - Footer | Component | Landing Page

--- src/components/landingPage/Footer.tsx
+++ src/components/landingPage/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="container mx-auto my-10 px-5">
+    <footer className="container mx-auto mt-10 px-5 pb-10">
       <div className="flex h-28 w-full items-center justify-center rounded-xl border border-black/50 bg-red-600 px-5 text-white shadow-md shadow-black/50 dark:border-white/50 dark:shadow-white/50 ">
         <p className="text-lg font-bold sm:text-xl md:text-2xl lg:text-3xl">{`WE'RE LOOKING FORWARD TO HEARING FROM YOU!`}</p>
       </div>
