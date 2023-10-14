9e8207773d226ac32826b8e9f267eca8c7aabe3b
gdwmw <gededewowahyumw@gmail.com>
2023-10-15 01:50:36 +0700

    feat - Footer | Component | Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 // IMPORT COMPONENTS
 import Header from "@/components/landingPage/Header";
 import Main from "@/components/landingPage/Main";
+import Footer from "@/components/landingPage/Footer";
 
 export default function page() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
