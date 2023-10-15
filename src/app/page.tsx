25c692bf3a676be7a1fc2be68cb27aa4e6ed8790
gdwmw <gededewowahyumw@gmail.com>
2023-10-15 13:23:14 +0700

    update - Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ import Header from "@/components/landingPage/Header";
 import Main from "@/components/landingPage/Main";
 import Footer from "@/components/landingPage/Footer";
 
-export default function Page() {
+export default function page() {
   return (
-      <>
-        <Header />
-        <Main />
-        <Footer />
-      </>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
