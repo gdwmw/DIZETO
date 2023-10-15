3bfa320375e15055a6f3c5714478c60ab38f20f1
gdwmw <gededewowahyumw@gmail.com>
2023-10-16 04:39:34 +0700

    update - Style Class Name | Navbar | Landing Page

--- src/components/landingPage/navbar/NavbarLoading.tsx
+++ src/components/landingPage/navbar/NavbarLoading.tsx
@@ -6,7 +6,7 @@ export default function NavbarLoading() {
     <div className="fixed left-0 top-0 z-20 flex h-16 w-full items-center justify-between px-10">
       <Image src={require("@/assets/images/logo/dizeto.svg")} alt="DIZETO" width={40} height={40} quality={50} priority={true} />
       <div className="flex gap-10 text-lg font-bold dark:text-white">
-        <div className="navbar-loading" />
+        <div className="navbar-loading-animation" />
       </div>
       <div className="h-10 w-10">
         <Image src={require("@/assets/loading/loading.svg")} alt="Loading" height={40} width={40} quality={50} />
