b27813211e0afda5ba30ba2509b69503130aed0c
gdwmw <gededewowahyumw@gmail.com>
2023-10-15 17:16:27 +0700

    update - Add Iframe Title | Main | Landing Page

--- src/components/landingPage/Main.tsx
+++ src/components/landingPage/Main.tsx
@@ -300,6 +300,7 @@ export default function Main() {
           </h2>
 
           <iframe
+            title="Google Maps"
             loading="lazy"
             className="my-5 h-[500px] w-full rounded-md border border-black dark:border-white"
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15844.143343315141!2d107.6504268!3d-6.8863111!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e76a1e6f56f3%3A0x8649ff7558d15108!2sDIZETO!5e0!3m2!1sid!2sid!4v1697306682702!5m2!1sid!2sid"
