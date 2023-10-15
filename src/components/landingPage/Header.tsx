a05661ad8253ae7f4a47a30b18f6a8fadbcf69cd
gdwmw <gededewowahyumw@gmail.com>
2023-10-16 00:56:43 +0700

    perf - Navbar | Header | Landing Page

--- src/components/landingPage/Header.tsx
+++ src/components/landingPage/Header.tsx
@@ -1,14 +1,31 @@
-// IMPORT COMPONENTS
+import { useEffect, useState } from "react";
 import NavbarMobile from "./navbar/NavbarMobile";
 import NavbarWebsite from "./navbar/NavbarWebsite";
+import NavbarLoading from "./navbar/NavbarLoading";
 
 export default function Header() {
+  const [navbar, setNavbar] = useState<React.ReactNode>(<NavbarLoading />);
+
+  const handleResize = () => {
+    const newWidth = window.innerWidth;
+    if (newWidth >= 840) {
+      setNavbar(<NavbarWebsite />);
+    } else {
+      setNavbar(<NavbarMobile />);
+    }
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <header id="Top">
-      <nav>
-        <NavbarWebsite />
-        <NavbarMobile />
-      </nav>
+      <nav>{navbar}</nav>
     </header>
   );
 }
