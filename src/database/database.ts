030d4ffe842906234178b55a9f39f51394f22f99
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2023-10-21 01:11:28 +0700

    fix - Database

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,3 +1,18 @@
+const landingPage = [
+  "DZT_CC1.webp",
+  "DZT_CC2.webp",
+  "DZT_CC3.webp",
+  "DZT_CC4.webp",
+  "DZT_CC5.webp",
+  "DZT_CC6.webp",
+  "DZT_CC7.webp",
+  "DZT_CC8.webp",
+  "DZT_CC9.webp",
+  "DZT_CC10.webp",
+  "DZT_CC11.webp",
+  "DZT_CC12.webp",
+];
+
 const listPortfolio = [
   {
     id: 1,
@@ -140,4 +155,4 @@ const sortedListPortfolio = listPortfolio.sort((a, b) => {
   return 0;
 });
 
-export { sortedListPortfolio };
+export { landingPage, sortedListPortfolio };
