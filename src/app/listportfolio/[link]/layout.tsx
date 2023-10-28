b20256e06678864ed6bb12124dc890fecea01d87
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2023-10-29 02:47:28 +0700

    update - Portfolio Layout

--- src/app/listportfolio/[link]/layout.tsx
+++ src/app/listportfolio/[link]/layout.tsx
@@ -1,3 +1,15 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  viewport: { width: "device-width", initialScale: 0.5 },
+  creator: "Gede Dewo Wahyu M.W",
+  authors: { name: "Gede Dewo Wahyu M.W", url: "https://github.com/gdwmw" },
+  applicationName: "DIZETO",
+  publisher: "Vercel",
+  robots: { index: true, follow: true },
+  title: "DIZETO | PORTFOLIO",
+};
+
 export default function Portfoliolayout({ children }: { children: React.ReactNode }) {
   return children;
 }
