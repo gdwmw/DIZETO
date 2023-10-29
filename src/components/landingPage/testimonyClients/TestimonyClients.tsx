263c2f1d08ba63d2bc255e9bea5644eb51aa5076
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2023-10-30 00:57:09 +0700

    update - Testimony Clients

--- src/components/landingPage/testimonyClients/TestimonyClients.tsx
+++ src/components/landingPage/testimonyClients/TestimonyClients.tsx
@@ -10,14 +10,12 @@ import rbwGray from "@/assets/images/clients/rbw-gray.svg";
 import rbwWhite from "@/assets/images/clients/rbw-white.svg";
 import sk from "@/assets/images/clients/sk.png";
 import loadingAnimation from "@/assets/loading/loading.svg";
+import { dbTestimonyClients } from "@/database/database";
 import { useTheme } from "next-themes";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { FaQuoteLeft, FaRegThumbsUp, FaToolbox, FaUserAlt } from "react-icons/fa";
 import { GoDotFill } from "react-icons/go";
-import { dbTestimonyClients } from "@/database/database";
-
-// TODO Optimasi dan perbaiki kode ini
 
 export default function TestimonyClients() {
   const [mounted, setMounted] = useState<boolean>(false);
@@ -65,82 +63,71 @@ export default function TestimonyClients() {
       clearInterval(dateTimeInterval);
     };
   }, []);
+
   return (
     <section id="Testimony" className="scroll-mt-[84px]">
       <div className="paper">
-        <div className="flex h-[450px] w-full flex-col items-center justify-center gap-5 dark:text-white">
-          <FaQuoteLeft size={30} />
-
-          <Image
-            src={require(`@/assets/images/testimony/${dbTestimonyClients[testimonyIndex].image}`)}
-            alt="Testimony"
-            height={128}
-            width={128}
-            quality={30}
-            loading="lazy"
-            className="rounded-full"
-          />
-
-          <div className="text-center">
-            <h3 className="text-lg font-semibold">{dbTestimonyClients[testimonyIndex].name}</h3>
-            <p className="text-sm font-semibold text-red-600">{dbTestimonyClients[testimonyIndex].status}</p>
+        <section>
+          <div className="flex h-[450px] w-full flex-col items-center justify-center gap-5 dark:text-white">
+            <FaQuoteLeft size={30} />
+            <Image
+              src={require(`@/assets/images/testimony/${dbTestimonyClients[testimonyIndex].image}`)}
+              alt="Testimony"
+              height={128}
+              width={128}
+              quality={30}
+              loading="lazy"
+              className="rounded-full"
+            />
+            <div className="text-center">
+              <h3 className="text-lg font-semibold">{dbTestimonyClients[testimonyIndex].name}</h3>
+              <p className="text-sm font-semibold text-red-600">{dbTestimonyClients[testimonyIndex].status}</p>
+            </div>
+            <p className="h-12 w-[500px] text-center font-semibold">{`"${dbTestimonyClients[testimonyIndex].comment}"`}</p>
+            <div className="mt-1 flex items-center justify-center gap-1">
+              {dbTestimonyClients.map((_, index) => (
+                <div key={index} className={testimonyIndex === index ? "text-red-600" : "text-red-300"}>
+                  <GoDotFill size={25} />
+                </div>
+              ))}
+            </div>
           </div>
-
-          <p className="h-12 w-[500px] text-center font-semibold">{`"${dbTestimonyClients[testimonyIndex].comment}"`}</p>
-
-          <div className="mt-1 flex items-center justify-center gap-1">
-            {dbTestimonyClients.map((_, index) => (
-              <div key={index} className={testimonyIndex === index ? "text-red-600" : "text-red-300"}>
-                <GoDotFill size={25} />
+          <div className="flex h-32 w-full items-center justify-evenly bg-white/50 dark:bg-dark/50">
+            <div className="flex flex-col items-center justify-center">
+              <div className="flex items-center justify-center gap-2">
+                <FaRegThumbsUp size={25} />
+                <p className="text-2xl font-bold text-red-600">48</p>
               </div>
-            ))}
-          </div>
-        </div>
-
-        <div className="flex h-32 w-full items-center justify-evenly bg-white/50 dark:bg-dark/50">
-          <div className="flex flex-col items-center justify-center">
-            <div className="flex items-center justify-center gap-2">
-              <FaRegThumbsUp size={25} />
-              <p className="text-2xl font-bold text-red-600">48</p>
+              <h4 className="text-xl font-semibold">Happy Client</h4>
             </div>
-            <h4 className="text-xl font-semibold">Happy Client</h4>
-          </div>
-
-          <div className="h-16 w-0.5 rounded-full bg-black dark:bg-white" />
-
-          <div className="flex flex-col items-center justify-center">
-            <div className="flex items-center justify-center gap-2">
-              <FaToolbox size={25} />
-              <p className="text-2xl font-bold text-red-600">50</p>
+            <div className="h-16 w-0.5 rounded-full bg-black dark:bg-white" />
+            <div className="flex flex-col items-center justify-center">
+              <div className="flex items-center justify-center gap-2">
+                <FaToolbox size={25} />
+                <p className="text-2xl font-bold text-red-600">50</p>
+              </div>
+              <h4 className="text-xl font-semibold">Completed Projects</h4>
             </div>
-            <h4 className="text-xl font-semibold">Completed Projects</h4>
-          </div>
-
-          <div className="h-16 w-0.5 rounded-full bg-black dark:bg-white" />
-
-          <div className="flex flex-col items-center justify-center">
-            <div className="flex items-center justify-center gap-2">
-              <FaUserAlt size={25} />
-              <p className="text-2xl font-bold text-red-600">8</p>
+            <div className="h-16 w-0.5 rounded-full bg-black dark:bg-white" />
+            <div className="flex flex-col items-center justify-center">
+              <div className="flex items-center justify-center gap-2">
+                <FaUserAlt size={25} />
+                <p className="text-2xl font-bold text-red-600">8</p>
+              </div>
+              <h4 className="text-xl font-semibold">Subscribers</h4>
             </div>
-            <h4 className="text-xl font-semibold">Subscriber</h4>
+            <div className="hidden h-16 w-0.5 rounded-full bg-black dark:bg-white lg:block" />
+            <div className="hidden flex-col items-center justify-center lg:flex">
+              <p className="w-[250px] text-center text-xl font-bold text-red-600">{dateTime}</p>
+              <h4 className="text-xl font-semibold">Date - Time</h4>
+            </div>
           </div>
-
-          <div className="hidden h-16 w-0.5 rounded-full bg-black dark:bg-white lg:block" />
-
-          <div className="hidden flex-col items-center justify-center lg:flex">
-            <p className="w-[250px] text-center text-xl font-bold text-red-600">{dateTime}</p>
-            <h4 className="text-xl font-semibold">Date - Time</h4>
-          </div>
-        </div>
-
-        {/* CLIENTS */}
-        <div id="Clients" className="scroll-mt-[84px] bg-white py-14 dark:bg-dark">
+        </section>
+        <section id="Clients" className="scroll-mt-[84px] bg-white py-14 dark:bg-dark">
           <h2 className="text-center text-3xl font-semibold">
-            CLIEN<span className="text-red-600">TS</span>
+            CLIENT<span className="text-red-600">S</span>
             <div className="mx-auto h-0.5 w-20 rounded-full bg-red-600" />
           </h2>
-
           {mounted ? (
             <div className="mt-5 space-y-5">
               <div className="flex items-center justify-evenly">
@@ -196,7 +183,7 @@ export default function TestimonyClients() {
           ) : (
             <Image src={loadingAnimation} alt="Loading" width={100} quality={30} className="mx-auto my-20" />
           )}
-        </div>
+        </section>
       </div>
     </section>
   );
