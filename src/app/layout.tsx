b977453b8cdeb52be82d926530b8d28ba21fd7f4
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2023-10-30 01:29:27 +0700

    update - Root Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 0.5,
