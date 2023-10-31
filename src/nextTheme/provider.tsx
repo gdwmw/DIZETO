e700bd0dc73beb29a94a74829a6087b3e6b4bfe8
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2023-10-31 21:22:15 +0700

    update - Next Theme Provider

--- src/nextTheme/provider.tsx
+++ src/nextTheme/provider.tsx
@@ -2,7 +2,7 @@
 
 import { ThemeProvider } from "next-themes";
 
-export function NextThemeProvider({ children }) {
+export function NextThemeProvider({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" enableSystem enableColorScheme={false}>
       {children}
