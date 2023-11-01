45be5c7d3dd2b48b28a49b66e81c8e1c977bdf72
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2023-11-01 23:34:43 +0700

    update - Main | Landing Page

--- src/components/landingPage/Main.tsx
+++ src/components/landingPage/Main.tsx
@@ -5,8 +5,6 @@ import Portfolio from "./portfolio/Portfolio";
 import Pricing from "./pricing/Pricing";
 import TestimonyClients from "./testimonyClients/TestimonyClients";
 
-// TODO Perbaiki type data
-
 export default function Main() {
   return (
     <main>
