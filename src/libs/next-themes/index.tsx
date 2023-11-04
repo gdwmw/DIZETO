5366f162c6697742828f9432b57e8aaf6b4f145c
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2023-11-04 14:05:13 +0700

    update - Next Themes

--- src/libs/next-themes/index.tsx
+++ src/libs/next-themes/index.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { ReactNode } from "react";
 
-export function NextThemesProvider({ children }: { children: React.ReactNode }): JSX.Element {
+export function NextThemesProvider({ children }: { children: ReactNode }): JSX.Element {
   return (
     <ThemeProvider attribute="class" enableSystem enableColorScheme={false}>
       {children}
