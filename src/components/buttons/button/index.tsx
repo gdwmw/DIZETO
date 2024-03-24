2afef0db2ffbc33f857be772e63c5fbef2e002b1
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-03-24 14:56:24 +0700

    update - Buttons | Component

--- src/components/buttons/button/index.tsx
+++ src/components/buttons/button/index.tsx
@@ -2,16 +2,7 @@ import { cn } from "@/libs";
 import { cva, type VariantProps } from "class-variance-authority";
 import { ButtonHTMLAttributes, FC, ReactElement } from "react";
 
-const ButtonCVA = cva("rounded-md border-2 border-red-600 font-semibold text-red-600 hover:bg-red-600 hover:text-white", {
-  variants: {
-    variant: {
-      usePadding: "px-4 py-2",
-    },
-    defaultVariants: {
-      variant: "usePadding",
-    },
-  },
-});
+const ButtonCVA = cva("rounded-md border-2 border-red-600 font-semibold px-4 py-2 text-red-600 hover:bg-red-600 hover:text-white");
 
 interface I extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof ButtonCVA> {}
 
