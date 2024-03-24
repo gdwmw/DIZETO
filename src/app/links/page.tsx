b8042a97aebd94051925e70885979d59f6e16b5e
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-03-24 14:49:21 +0700

    update - Links

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -1,6 +1,7 @@
+import { ButtonCVA } from "@/components";
 import logoDIZETO from "@/public/assets/images/logos/dizeto.webp";
-import { AncorButton } from "@/components/links";
 import Image from "next/image";
+import Link from "next/link";
 import { FC, ReactElement } from "react";
 import { AiFillInstagram } from "react-icons/ai";
 import { BsFacebook, BsTwitter, BsYoutube } from "react-icons/bs";
@@ -22,12 +23,46 @@ const Links: FC = (): ReactElement => {
             </div>
           </header>
           <main className="space-y-5">
-            <AncorButton icon={<TbWorldWww />} hrf="/" label="WEBSITE" />
-            <AncorButton icon={<IoLogoWhatsapp />} hrf="/" label="WHATSAPP" />
-            <AncorButton icon={<AiFillInstagram />} hrf="https://instagram.com/dizeto_id?igshid=YmMyMTA2M2Y=" label="INSTAGRAM" />
-            <AncorButton icon={<BsYoutube />} hrf="https://www.youtube.com/@dizeto" label="YOUTUBE" />
-            <AncorButton icon={<BsTwitter />} hrf="https://twitter.com/dizeto_id" label="TWITTER" />
-            <AncorButton icon={<BsFacebook />} hrf="https://www.facebook.com/profile.php?id=100083806837197" label="FACEBOOK" />
+            <Link href={"/"} className={ButtonCVA({ className: "flex items-center justify-center gap-1" })}>
+              <TbWorldWww />
+              WEBSITE
+            </Link>
+            <Link href={"/links"} className={ButtonCVA({ className: "flex items-center justify-center gap-1" })}>
+              <IoLogoWhatsapp />
+              WHATSAPP
+            </Link>
+            <Link
+              href={"https://instagram.com/dizeto_id?igshid=YmMyMTA2M2Y="}
+              target="_blank"
+              className={ButtonCVA({ className: "flex items-center justify-center gap-1" })}
+            >
+              <AiFillInstagram />
+              INSTAGRAM
+            </Link>
+            <Link
+              href={"https://www.youtube.com/@dizeto"}
+              target="_blank"
+              className={ButtonCVA({ className: "flex items-center justify-center gap-1" })}
+            >
+              <BsYoutube />
+              YOUTUBE
+            </Link>
+            <Link
+              href={"https://twitter.com/dizeto_id"}
+              target="_blank"
+              className={ButtonCVA({ className: "flex items-center justify-center gap-1" })}
+            >
+              <BsTwitter />
+              TWITTER
+            </Link>
+            <Link
+              href={"https://www.facebook.com/profile.php?id=100083806837197"}
+              target="_blank"
+              className={ButtonCVA({ className: "flex items-center justify-center gap-1" })}
+            >
+              <BsFacebook />
+              FACEBOOK
+            </Link>
           </main>
         </div>
         <footer className="w-full">
