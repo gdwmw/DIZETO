897c17cec798f3b84eb341bea5736cc5e76dcdf6
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-04-11 19:57:36 +0700

    update - Highlight Portfolio | Landing Page

--- src/modules/landing-page/main/highlight-portfolio/index.tsx
+++ src/modules/landing-page/main/highlight-portfolio/index.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 import { FC, ReactElement } from "react";
 import { ImagesFrame } from "./images-frame";
 
-export const Portfolio: FC = (): ReactElement => {
+export const HightlightPortfolio: FC = (): ReactElement => {
   return (
     <section id="Portfolio" className="scroll-mt-[84px]">
       <div className="paper bg-white p-5 dark:bg-dark">
         <h2 className="text-center text-3xl font-semibold">
-          PORTFOL<span className="text-red-600">IO</span>
+          HIGHLIGHT <span className="text-red-600">PORTFOLIO</span>
           <div className="mx-auto h-0.5 w-20 rounded-full bg-red-600" />
         </h2>
         <div className="my-5 flex items-center justify-center">
