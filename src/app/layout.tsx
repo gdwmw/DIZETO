9b7bf46dbd4f466166b22347cb41d75126c99ffb
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-06-06 23:12:48 +0700

    refactor: Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,48 +1,25 @@
+import type { Metadata, Viewport } from "next";
+import { FC, ReactElement, ReactNode } from "react";
+import { Inter } from "next/font/google";
 import { NextThemesProvider } from "@/libs";
 import "@/styles/globals.css";
-import type { Metadata, Viewport } from "next";
-import { Inter } from "next/font/google";
-import { FC, ReactElement, ReactNode } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const viewport: Viewport = {
-  width: "device-width",
   initialScale: 0.5,
+  width: "device-width",
 };
 
 export const metadata: Metadata = {
+  authors: [{ name: "Gede Dewo Wahyu M.W", url: "https://github.com/gdwmw" }],
+  category: "Photography, Videography, Talent, Music Services",
   creator: "Gede Dewo Wahyu M.W",
-  authors: { name: "Gede Dewo Wahyu M.W", url: "https://github.com/gdwmw" },
-  applicationName: "DIZETO",
-  publisher: "Vercel",
-  robots: { index: true, follow: true },
-  title: "DIZETO | PROFESSIONAL SERVICES",
-  description: "Dizeto is a vendor that offers photography, videography, talent, and music services.",
-  keywords:
-    "dizeto, bandung, indonesia, vendor, jasa foto, jasa video, jasa fotografer, jasa videografer, pre-wedding, wedding, dokumentasi, foto produk, fotografi, videografi",
-  metadataBase: new URL("https://dizeto.vercel.app/"),
-  openGraph: {
-    type: "website",
-    url: "https://dizeto.vercel.app/",
-    title: "DIZETO | PROFESSIONAL SERVICES",
-    description: "Dizeto is a vendor that offers photography, videography, talent, and music services.",
-    siteName: "DIZETO",
-    images: [
-      {
-        url: "https://dizeto.vercel.app/assets/images/logo/dizeto.webp",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@dizeto_id",
-    creator: "@dizeto_id",
-    title: "DIZETO | PROFESSIONAL SERVICES",
-    description: "Dizeto is a vendor that offers photography, videography, talent, and music services.",
-    images: {
-      url: "https://dizeto.vercel.app/assets/images/logo/dizeto.webp",
-    },
+  publisher: "Gede Dewo Wahyu M.W",
+  referrer: "strict-origin-when-cross-origin",
+  title: {
+    default: "DIZETO | PROFESSIONAL SERVICES",
+    template: "DIZETO | %s",
   },
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,63 @@
+import { FC, ReactElement } from "react";
+import { Metadata } from "next";
 import { Footer } from "@/components";
 import { Header, Main } from "@/modules/landing-page";
-import { FC, ReactElement } from "react";
+
+export const metadata: Metadata = {
+  description: "DIZETO is a vendor that offers photography, videography, talent, and music services.",
+  keywords: [
+    "dizeto",
+    "bandung",
+    "indonesia",
+    "vendor",
+    "jasa foto",
+    "jasa video",
+    "jasa fotografer",
+    "jasa videografer",
+    "pre-wedding",
+    "wedding",
+    "dokumentasi",
+    "foto produk",
+    "fotografi",
+    "videografi",
+  ],
+  openGraph: {
+    description: "DIZETO is a vendor that offers photography, videography, talent, and music services.",
+    images: [
+      {
+        alt: "DIZETO",
+        height: 800,
+        url: "https://dizeto.vercel.app/assets/images/logos/dizeto.png",
+        width: 800,
+      },
+    ],
+    locale: "en_US",
+    siteName: "DIZETO",
+    title: "DIZETO | PROFESSIONAL SERVICES",
+    type: "website",
+    url: "https://dizeto.vercel.app/",
+  },
+  robots: {
+    follow: true,
+    googleBot: {
+      follow: true,
+      index: true,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+      "max-video-preview": -1,
+      noimageindex: false,
+    },
+    index: true,
+    nocache: false,
+  },
+  twitter: {
+    card: "summary_large_image",
+    creator: "@gdwmw",
+    description: "DIZETO is a vendor that offers photography, videography, talent, and music services.",
+    images: ["https://dizeto.vercel.app/assets/images/logos/dizeto.png"],
+    title: "DIZETO | PROFESSIONAL SERVICES",
+  },
+};
 
 const LandingPage: FC = (): ReactElement => {
   return (
