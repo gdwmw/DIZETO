aa1011fe0bb66c138661c4e94a61f963e0f579d7
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-06-07 16:11:40 +0700

    refactor: Update List Portfolio page metadata

--- src/app/listportfolio/layout.tsx
+++ src/app/listportfolio/layout.tsx
@@ -1,45 +1,10 @@
-import type { Metadata, Viewport } from "next";
+import type { Viewport } from "next";
 
 import { FC, ReactNode } from "react";
 
 export const viewport: Viewport = {
   initialScale: 0.5,
   width: "device-width",
-};
-
-export const metadata: Metadata = {
-  applicationName: "DIZETO",
-  authors: { name: "Gede Dewo Wahyu M.W", url: "https://github.com/gdwmw" },
-  creator: "Gede Dewo Wahyu M.W",
-  description: "See our portfolio for examples of our work. We believe that our portfolio demonstrates our abilities and creativity.",
-  keywords:
-    "dizeto list portfolio, bandung, indonesia, vendor, jasa foto, jasa video, jasa fotografer, jasa videografer, pre-wedding, wedding, dokumentasi, foto produk, fotografi, videografi",
-  metadataBase: new URL("https://dizeto.vercel.app/listportfolio"),
-  openGraph: {
-    description: "See our portfolio for examples of our work. We believe that our portfolio demonstrates our abilities and creativity.",
-    images: [
-      {
-        url: "https://dizeto.vercel.app/assets/images/logo/dizeto.webp",
-      },
-    ],
-    siteName: "DIZETO",
-    title: "DIZETO | LIST PORTFOLIO",
-    type: "website",
-    url: "https://dizeto.vercel.app/listportfolio",
-  },
-  publisher: "Vercel",
-  robots: { follow: true, index: true },
-  title: "DIZETO | LIST PORTFOLIO",
-  twitter: {
-    card: "summary_large_image",
-    creator: "@dizeto_id",
-    description: "See our portfolio for examples of our work. We believe that our portfolio demonstrates our abilities and creativity.",
-    images: {
-      url: "https://dizeto.vercel.app/assets/images/logo/dizeto.webp",
-    },
-    site: "@dizeto_id",
-    title: "DIZETO | LIST PORTFOLIO",
-  },
 };
 
 const ListPortfolioLayout: FC<{ children: ReactNode }> = ({ children }): ReactNode => {
--- src/app/listportfolio/page.tsx
+++ src/app/listportfolio/page.tsx
@@ -1,6 +1,50 @@
 import { FC, ReactElement } from "react";
 
+import { Metadata } from "next";
+
 import { ListPortfolio as ListPort } from "@/modules/list-portfolio";
+
+export const metadata: Metadata = {
+  description: "See our portfolio for examples of our work. We believe that our portfolio demonstrates our abilities and creativity.",
+  keywords: ["dizeto", "list portfolio", "dizeto list portfolio"],
+  openGraph: {
+    description: "See our portfolio for examples of our work. We believe that our portfolio demonstrates our abilities and creativity.",
+    images: [
+      {
+        alt: "DIZETO",
+        height: 800,
+        url: "https://dizeto.vercel.app/assets/images/logos/dizeto.png",
+        width: 800,
+      },
+    ],
+    locale: "en_US",
+    siteName: "DIZETO",
+    title: "DIZETO | LIST PORTFOLIO",
+    type: "website",
+    url: "https://dizeto.vercel.app/listportfolio",
+  },
+  robots: {
+    follow: true,
+    googleBot: {
+      follow: true,
+      index: true,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+      "max-video-preview": -1,
+      noimageindex: false,
+    },
+    index: true,
+    nocache: false,
+  },
+  title: "LIST PORTFOLIO",
+  twitter: {
+    card: "summary_large_image",
+    creator: "@gdwmw",
+    description: "See our portfolio for examples of our work. We believe that our portfolio demonstrates our abilities and creativity.",
+    images: ["https://dizeto.vercel.app/assets/images/logos/dizeto.png"],
+    title: "DIZETO | LIST PORTFOLIO",
+  },
+};
 
 const ListPortfolio: FC = (): ReactElement => {
   return <ListPort />;
