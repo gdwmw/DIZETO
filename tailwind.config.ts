fbd8fbb1ea2cac31d7d84f5041aef788989f3173
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-06-17 03:44:03 +0700

    feat: Add dark color to theme in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,14 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./src/**/*.{js,jsx,mdx,ts,tsx}"],
-  darkMode: ['selector', '[data-theme="dark"]'],
+  darkMode: ["selector", '[data-theme="dark"]'],
   plugins: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#23272d",
+      },
+    },
   },
 };
 
