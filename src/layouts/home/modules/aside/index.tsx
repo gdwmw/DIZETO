6f71743ac31c4a3d433a2268c0c5161c0e928447
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-06-17 09:56:58 +0700

    refactor: Update aside component padding

--- src/layouts/home/modules/aside/index.tsx
+++ src/layouts/home/modules/aside/index.tsx
@@ -15,7 +15,7 @@ export const ASide: FC = (): ReactElement => {
 
   return (
     <aside
-      className={`fixed right-0 top-0 h-dvh w-full max-w-[250px] bg-black/10 p-10 shadow-black/50 backdrop-blur-md transition-all dark:bg-white/10 dark:shadow-white/50 ${openASide ? "translate-x-0 shadow-md" : "translate-x-full shadow-none"}`}
+      className={`fixed right-0 top-0 h-dvh w-full max-w-[250px] bg-black/10 p-5 shadow-black/50 backdrop-blur-md transition-all dark:bg-white/10 dark:shadow-white/50 ${openASide ? "translate-x-0 shadow-md" : "translate-x-full shadow-none"}`}
     >
       <nav className="relative">
         <button
