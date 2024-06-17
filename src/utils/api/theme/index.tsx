987232e9b8c9b3064ea6fba8d61ae621d0e6a6ec
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-06-17 16:43:02 +0700

    refactor: Update API response type in PUTTheme function

--- src/utils/api/theme/index.tsx
+++ src/utils/api/theme/index.tsx
@@ -18,7 +18,7 @@ export const PUTTheme = async (data: string): Promise<string> => {
       throw new Error(`Failed to put: Theme with status ${res.status}`);
     }
 
-    return await res.json();
+    return await res.text();
   } catch (error) {
     console.log(error);
     throw error;
