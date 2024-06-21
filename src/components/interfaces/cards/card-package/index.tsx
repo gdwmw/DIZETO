b83dd1eb6053406bfc3439e3a3cccbd98d59f105
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-06-21 22:01:09 +0700

    comp: Card Package

---
+++ src/components/interfaces/cards/card-package/index.tsx
@@ -0,0 +1,46 @@
+import { FC, ReactElement } from "react";
+
+import { IPricing } from "@/utils";
+
+export const CardPackage: FC<IPricing> = ({ ...data }): ReactElement => {
+  return (
+    <section className="size-full pt-20">
+      <div className="relative size-full rounded-lg transition-card hover:scale-105 hover:shadow-md hover:shadow-black/50 dark:hover:shadow-white/50">
+        <div className="absolute top-[-70px] flex h-fit w-full items-center justify-center">
+          <div className="flex size-[138px] flex-col items-center justify-center rounded-full border-4 border-gray-200 bg-red-600 text-center text-white dark:border-gray-700">
+            <span className="text-3xl font-bold">IDR {data.price}</span>
+            <span className="font-semibold tracking-wider">PACKAGE {data.pack}</span>
+          </div>
+        </div>
+
+        <div className="size-full overflow-hidden rounded-lg border-4 border-gray-200 bg-white dark:border-gray-700 dark:bg-dark">
+          <div className="bg-gray-200 pb-5 pt-20 dark:bg-gray-700">
+            <span className="block whitespace-pre-wrap text-center text-2xl font-bold">{data.category}</span>
+          </div>
+
+          <ul className="space-y-5 p-5 text-center text-lg font-semibold">
+            {data.listItem.map((dt) =>
+              dt.list.map((ls, index) => (
+                <li key={index}>
+                  <span>
+                    <span className="text-red-600">{ls.qty}</span> {ls.label}
+                  </span>
+                </li>
+              )),
+            )}
+
+            <li>
+              <span>
+                SENT VIA
+                <br />
+                GOOGLE DRIVE
+                <br />
+                (EXPIRED <span className="text-red-600">7</span> DAYS)
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
