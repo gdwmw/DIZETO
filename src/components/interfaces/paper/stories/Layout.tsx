7518954d7c51861edf81995d92ab402362cf697b
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-06-23 12:59:05 +0700

    refactor: Update stories of Paper component

--- src/components/interfaces/paper/stories/Layout.tsx
+++ src/components/interfaces/paper/stories/Layout.tsx
@@ -1,15 +1,42 @@
 import { FC, ReactElement, ReactNode } from "react";
 
+import Image from "next/image";
+
+import logoDIZETO from "@/public/assets/images/logos/dizeto.svg";
+
 import { ContainerPaper, ContentPaper } from "..";
+import { Title } from "../../title";
 
 type TLayout = {
   children: ReactNode;
+  id: string;
 };
 
 const Layout: FC<TLayout> = ({ ...props }): ReactElement => {
   return (
-    <ContainerPaper>
-      <ContentPaper>{props.children}</ContentPaper>
+    <ContainerPaper id={props.id}>
+      <ContentPaper>
+        <Title title="ABO" titleRed="UT" />
+        <div className="space-y-14 py-10 md:grid md:grid-cols-2 md:space-y-0">
+          <div className="space-y-5">
+            <h3 className="text-xl font-semibold sm:text-2xl">
+              <span className="text-red-600">W</span>hat is Dizeto?
+            </h3>
+            <p className="text-justify text-sm font-semibold sm:text-base">
+              <span className="text-red-600">Dizeto</span> is a vendor that offers photography, videography, talent, and music services. We have a
+              professional team that can help you meet business needs, events, and your special moments to make them more beautiful, real, and
+              lasting. You can learn more about us by viewing our portfolio, YouTube channel, and customer testimonials.
+            </p>
+            <p className="pt-5 text-sm font-semibold text-red-600 sm:text-base">
+              *Please take note that we currently only offer photography and videography services.
+            </p>
+          </div>
+          <div className="flex items-center justify-center">
+            <Image alt="DIZETO" height={250} priority src={logoDIZETO} width={250} />
+          </div>
+        </div>
+        {props.children}
+      </ContentPaper>
     </ContainerPaper>
   );
 };
--- src/components/interfaces/paper/stories/Paper.stories.tsx
+++ src/components/interfaces/paper/stories/Paper.stories.tsx
@@ -19,7 +19,7 @@ type Story = StoryObj<typeof Layout>;
 /* eslint-disable perfectionist/sort-objects */
 export const Primary: Story = {
   args: {
-    children: <p>This is example text</p>,
+    id: "example-id",
   },
 };
 /* eslint-enable perfectionist/sort-objects */
