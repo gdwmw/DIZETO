2afc07e3a55eeb4bffdf87a86ee0f74e561faa55
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-06-28 20:26:02 +0700

    feat: About Form

--- src/layouts/home/modules/main/batches/about/index.tsx
+++ src/layouts/home/modules/main/batches/about/index.tsx
@@ -1,13 +1,17 @@
 "use client";
 
-import { FC, ReactElement } from "react";
+import { FC, ReactElement, useState } from "react";
 
 import { useQuery } from "@tanstack/react-query";
+import dynamic from "next/dynamic";
 import Image from "next/image";
+import { FaEdit } from "react-icons/fa";
 
+import { Button } from "@/interfaces/buttons/button";
 import { ContainerPaper, ContentPaper } from "@/interfaces/paper";
 import { Title } from "@/interfaces/title";
 import { GETAbout, GETTitle } from "@/utils";
+const AboutForm = dynamic(() => import("../forms/about-form"));
 
 export const About: FC = (): ReactElement => {
   const { data: dataTitle } = useQuery({
@@ -19,27 +23,38 @@ export const About: FC = (): ReactElement => {
     queryKey: ["GETAbout"],
   });
 
+  const [openForm, setOpenForm] = useState(false);
+
   return (
-    <ContainerPaper id="about">
-      <ContentPaper>
-        <Title title={dataTitle?.[0].title} titleRed={dataTitle?.[0].titleRed} />
-        <div className="space-y-14 py-10 md:grid md:grid-cols-2 md:space-y-0">
-          <div className="space-y-5">
-            <h3 className="text-xl font-semibold sm:text-2xl">
-              <span className="text-red-600">{dataAbout?.subTitle.slice(0, 1)}</span>
-              {dataAbout?.subTitle.slice(1)}
-            </h3>
-            <p className="text-justify text-sm font-semibold sm:text-base">
-              <span className="text-red-600">{dataAbout?.description.slice(0, 6)}</span>
-              {dataAbout?.description.slice(6)}
-            </p>
-            <p className="pt-5 text-sm font-semibold text-red-600 sm:text-base">{dataAbout?.note}</p>
+    <>
+      <ContainerPaper id="about">
+        <ContentPaper className="relative">
+          <Button className="absolute right-3 top-3" color="black" onClick={() => setOpenForm(true)} size="sm" variant="ghost">
+            <FaEdit />
+          </Button>
+
+          <Title title={dataTitle?.[0].title} titleRed={dataTitle?.[0].titleRed} />
+
+          <div className="space-y-14 py-10 md:grid md:grid-cols-2 md:space-y-0">
+            <div className="space-y-5">
+              <h3 className="text-xl font-semibold sm:text-2xl">
+                <span className="text-red-600">{dataAbout?.subTitle.slice(0, 1)}</span>
+                {dataAbout?.subTitle.slice(1)}
+              </h3>
+              <p className="text-justify text-sm font-semibold sm:text-base">
+                <span className="text-red-600">{dataAbout?.description.slice(0, 6)}</span>
+                {dataAbout?.description.slice(6)}
+              </p>
+              <p className="pt-5 text-sm font-semibold text-red-600 sm:text-base">{dataAbout?.note}</p>
+            </div>
+            <div className="flex items-center justify-center">
+              <Image alt="DIZETO" height={250} priority src={dataAbout?.logoUrl ?? ""} width={250} />
+            </div>
           </div>
-          <div className="flex items-center justify-center">
-            <Image alt="DIZETO" height={250} priority src={dataAbout?.logoUrl ?? ""} width={250} />
-          </div>
-        </div>
-      </ContentPaper>
-    </ContainerPaper>
+        </ContentPaper>
+      </ContainerPaper>
+
+      {openForm && <AboutForm data={dataAbout} setOpenForm={setOpenForm} title={dataTitle?.[0]} />}
+    </>
   );
 };
---
+++ src/layouts/home/modules/main/batches/forms/about-form/index.tsx
@@ -0,0 +1,95 @@
+import { FC, ReactElement, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { Button } from "@/interfaces/buttons/button";
+import { Input } from "@/interfaces/inputs/input";
+import { TextArea } from "@/interfaces/inputs/text-area";
+import { ContainerModal, ContentModal } from "@/interfaces/modal";
+import { Title } from "@/interfaces/title";
+import { IAbout, ITitle, PUTAbout, PUTTitle } from "@/utils";
+
+import { Schema, TSchema } from "./Schema";
+
+type T = {
+  data: IAbout | undefined;
+  setOpenForm: (value: boolean) => void;
+  title: ITitle | undefined;
+};
+
+const AboutForm: FC<T> = ({ data, setOpenForm, title }): ReactElement => {
+  const queryClient = useQueryClient();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+    reset,
+  } = useForm<TSchema>({
+    defaultValues: { data: { ...data }, title: { ...title } },
+    resolver: zodResolver(Schema),
+  });
+
+  const handleUpdateTitle = useMutation({
+    mutationFn: PUTTitle,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["GETTitle"] });
+    },
+  });
+
+  const handleUpdate = useMutation({
+    mutationFn: PUTAbout,
+    onError: () => setLoading(false),
+    onMutate: () => setLoading(true),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["GETAbout"] });
+      setOpenForm(false);
+      setLoading(false);
+      reset();
+    },
+  });
+
+  const onSubmit: SubmitHandler<TSchema> = async (data) => {
+    handleUpdateTitle.mutate(data.title);
+    handleUpdate.mutate(data.data);
+  };
+
+  return (
+    <ContainerModal>
+      <ContentModal>
+        <Title title="UPDATE " titleRed="ABOUT" />
+        <form className="space-y-3" onSubmit={handleSubmit(onSubmit)}>
+          <Input color="black" errorMessage={errors.title?.title?.message} label="Title" {...register("title.title")} />
+          <Input color="black" errorMessage={errors.title?.titleRed?.message} label="Title Red" {...register("title.titleRed")} />
+          <Input color="black" errorMessage={errors.data?.subTitle?.message} label="Sub Title" {...register("data.subTitle")} />
+          <TextArea color="black" errorMessage={errors.data?.description?.message} label="Description" {...register("data.description")} />
+          <Input color="black" errorMessage={errors.data?.note?.message} label="Note" {...register("data.note")} />
+          <Input color="black" errorMessage={errors.data?.logoUrl?.message} label="Logo URL" {...register("data.logoUrl")} />
+          <div className="flex items-center gap-2 font-semibold">
+            <Button className="w-full" color="red" disabled={loading} size="sm" type="submit" variant="outline">
+              Update
+            </Button>
+            <Button
+              color="red"
+              disabled={loading}
+              onClick={() => {
+                reset();
+                setOpenForm(false);
+              }}
+              size="sm"
+              type="button"
+              variant="outline"
+            >
+              Cancel
+            </Button>
+          </div>
+        </form>
+      </ContentModal>
+    </ContainerModal>
+  );
+};
+
+export default AboutForm;
