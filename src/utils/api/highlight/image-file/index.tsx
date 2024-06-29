1ce5394bd686b70b8761034d7f3463a3b6038304
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-06-29 14:31:58 +0700

    fix: PUT ImageFile API Utils

--- src/utils/api/highlight/image-file/index.tsx
+++ src/utils/api/highlight/image-file/index.tsx
@@ -1,26 +1,30 @@
 import { IImageFile } from "../index";
 
-const API_URL = process.env.NEXT_PUBLIC_HIGHLIGHT + "/image-file";
+const API_URL = process.env.NEXT_PUBLIC_HIGHLIGHT;
 
 if (!API_URL) {
   throw new Error("The API URL is not defined. Please check your environment variables.");
 }
 
-export const PUTImageFile = async (data: IImageFile): Promise<IImageFile> => {
+export const PUTImageFile = async (data: IImageFile[]): Promise<IImageFile[]> => {
   try {
-    const res = await fetch(`${API_URL}/${data.id}`, {
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "PUT",
+    const promises = data.map(async (file) => {
+      const res = await fetch(`${API_URL}/imageFile/${file.id}`, {
+        body: JSON.stringify(file),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "PUT",
+      });
+
+      if (!res.ok) {
+        throw new Error(`Failed to put: Image File id ${file.id} with status ${res.status}`);
+      }
+
+      return await res.json();
     });
 
-    if (!res.ok) {
-      throw new Error(`Failed to put: Image File with status ${res.status}`);
-    }
-
-    return await res.json();
+    return await Promise.all(promises);
   } catch (error) {
     console.log(error);
     throw error;
