c17ab3b60dac331878497e05a989335c75d7cdab
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-07-25 23:43:40 +0700

    refactor: 💡 tidy up code

--- src/types/api/index.ts
+++ src/types/api/index.ts
@@ -1,61 +1,24 @@
+export interface IExample {
+  id: string;
+  title: string;
+}
+
+// ----------------------------
+
+export interface ITitle {
+  id: string;
+  title: string;
+  titleRed: string;
+}
+
+// ----------------------------
+
 export interface IAbout {
   description: string;
   id: string;
   logoURL: string;
   note: string;
   subTitle: string;
-}
-
-// ----------------------------
-
-export interface IAuthPayload {
-  confirmPassword?: string;
-  email?: string;
-  name?: string;
-  password: string;
-  username: string;
-}
-
-export interface IAuthResponse {
-  email: string;
-  id: string;
-  image?: null | string;
-  name: string;
-  role: string;
-  token: string;
-  username: string;
-}
-
-// ----------------------------
-
-export interface IClient {
-  alt: string;
-  href: string;
-  id: string;
-  logoURL: string;
-  theme: string;
-}
-
-// ----------------------------
-
-export interface IContact {
-  href: string;
-  label: string;
-}
-
-// ----------------------------
-
-export interface ICounting {
-  count: number;
-  id: string;
-  title: string;
-}
-
-// ----------------------------
-
-export interface IExample {
-  id: string;
-  title: string;
 }
 
 // ----------------------------
@@ -106,8 +69,45 @@ export interface ITestimony {
 
 // ----------------------------
 
-export interface ITitle {
+export interface ICounting {
+  count: number;
   id: string;
   title: string;
-  titleRed: string;
+}
+
+// ----------------------------
+
+export interface IClient {
+  alt: string;
+  href: string;
+  id: string;
+  logoURL: string;
+  theme: string;
+}
+
+// ----------------------------
+
+export interface IContact {
+  href: string;
+  label: string;
+}
+
+// ----------------------------
+
+export interface IAuthPayload {
+  confirmPassword?: string;
+  email?: string;
+  name?: string;
+  password: string;
+  username: string;
+}
+
+export interface IAuthResponse {
+  email: string;
+  id: string;
+  image?: null | string;
+  name: string;
+  role: string;
+  token: string;
+  username: string;
 }
