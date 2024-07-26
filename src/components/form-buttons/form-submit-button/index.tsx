70e1d94c433b46dac0bb0bd8bf945f5fe0c46722
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-07-26 11:40:52 +0700

    feat: 🎉 form submit button

---
+++ src/components/form-buttons/form-submit-button/index.tsx
@@ -0,0 +1,43 @@
+import { FC, ReactElement } from "react";
+
+import Image from "next/image";
+
+import loadingAnimation from "@/root/public/assets/animations/loadings/loading.svg";
+
+import { Button } from "../../interfaces/buttons/button";
+
+type T = {
+  loading: boolean;
+  onClick: () => void;
+  primaryLabel: string;
+  secondaryLabel: string;
+};
+
+export const FormSubmitButton: FC<T> = (props): ReactElement => {
+  return (
+    <div className="grid grid-cols-2 gap-5 font-semibold sm:flex sm:items-center">
+      <Button
+        className={`gap-1 sm:w-full ${props.loading ? "cursor-wait" : ""}`}
+        color="red"
+        disabled={props.loading}
+        size="sm"
+        type="submit"
+        variant="outline"
+      >
+        {props.loading && <Image alt="Loading..." className="h-auto" src={loadingAnimation} width={25} />}
+        {props.primaryLabel}
+      </Button>
+      <Button
+        className={props.loading ? "cursor-wait" : ""}
+        color="red"
+        disabled={props.loading}
+        onClick={props.onClick}
+        size="sm"
+        type="button"
+        variant="outline"
+      >
+        {props.secondaryLabel}
+      </Button>
+    </div>
+  );
+};
