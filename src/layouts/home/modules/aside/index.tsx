6254e1ff6c865bc8d0b520c7f75e682f71af2b26
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2024-07-29 08:47:31 +0700

    fix: 🔧 semantic structure

--- src/layouts/home/modules/aside/index.tsx
+++ src/layouts/home/modules/aside/index.tsx
@@ -18,7 +18,7 @@ export const ASide: FC = (): ReactElement => {
     <aside
       className={`fixed inset-y-0 right-0 z-[3] w-full max-w-[250px] bg-white/50 p-5 shadow-black/50 backdrop-blur-md transition-all dark:bg-black/50 dark:shadow-white/50 ${openASide ? "translate-x-0 shadow-md" : "translate-x-full shadow-none"}`}
     >
-      <nav className="relative">
+      <div className="relative">
         <button
           className={ButtonTWM({ className: "absolute right-0 top-0", color: "black", size: "sm", variant: "ghost" })}
           onClick={() => setOpenASide(false)}
@@ -42,7 +42,7 @@ export const ASide: FC = (): ReactElement => {
             </li>
           ))}
         </ul>
-      </nav>
+      </div>
     </aside>
   );
 };
