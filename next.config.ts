9c03a982c2fbe1d13b59e7150ba80dfeaf83b8cc
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2025-09-30 13:06:03 +0700

    chore: ⚙️ next config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev"],
   images: {
     remotePatterns: [
       {
         hostname: "localhost",
         pathname: "/**",
-        port: "1337",
+        port: "1339",
         protocol: "http",
         search: "",
       },
